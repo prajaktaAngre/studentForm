5f71c11dd564e0e313a612d7f6bd07bed655b02b
prajakta <prajakta.angre@iauro.com>
2021-09-27 13:25:44 +0530

    sidenav

--- src/app/sidenav/sidenav-form/sidenav-form.component.ts
+++ src/app/sidenav/sidenav-form/sidenav-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sidenav-form',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav-form.component.scss']
 })
 export class SidenavFormComponent implements OnInit {
+  @Output() newItemEvent = new EventEmitter<string>();//send data of onsubmit method
+
   showFiller = false;
   favoriteStream=[];
   streams: string[] = ['Computer', 'Mechanical', 'Electrical', 'IT'];
@@ -14,10 +17,52 @@ export class SidenavFormComponent implements OnInit {
                       {value:'writting blogs',label:'writting blogs'},
                       {value:'gyming',label:'gyming'},
                       {value:'coding',label:'coding'}];
-                      modeOn: boolean = true;
+  modeOn: boolean = true;
+  userOptions='';
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    stream: new FormControl('', Validators.required),
+    hobbies: new FormControl(''),
+    
+  });
+  submitted=true;
+  position: number = 5;
+  // methods
+  onSubmit(position: any) {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.profileForm.invalid) {
+      return;
+    }
+
+    else {
+
+      this.position = this.position + 1;
+      this.profileForm.value['position'] = this.position;
+      console.log (this.profileForm.value);
+      this.newItemEvent.emit(this.profileForm.value);
+       console.log("newitemevent",this.newItemEvent)
+     
+      this.profileForm.reset()
+    }
+
+  }
+  get f() { return this.profileForm.controls; }
+ 
+  addNewItem(value: string) {
+    this.newItemEvent.emit(value);
+    console.log("newitemevent",this.newItemEvent)
+
+
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+function value(value: any) {
+  throw new Error('Function not implemented.');
+}
+
--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -3,11 +3,11 @@ export interface student {
   name: string;
   position: number;
   stream: string;
-  hobbie: string;
+  hobbie: any;
 }
 
 const ELEMENT_DATA: student[] = [
-  {position: 1, name: 'Swarali', stream: 'Computer Science', hobbie: 'Cricket'},
+  {position: 1, name: 'Swarali', stream: 'Computer Science', hobbie: ['Cricket','sa']},
   {position: 2, name: 'Shweta', stream: 'Mechanical', hobbie: 'Dance'},
   {position: 3, name: 'Sayali', stream: 'BCS Science', hobbie: 'Singing'},
   {position: 4, name: 'Bela', stream: 'Electrical', hobbie: 'Ludo'},
@@ -21,9 +21,25 @@ const ELEMENT_DATA: student[] = [
 export class SidenavComponent implements OnInit {
   displayedColumns: string[] = ['position', 'name', 'stream', 'hobbie'];
   dataSource = ELEMENT_DATA;
+  items: any = [];
+  student: any = [];
+  // methods
+  addItem(newItem: any) {
+ 
+      this.dataSource.push(newItem);
+      console.log("data emits successuly", newItem)
+    console.log("items",this.dataSource)
+
+
+  }
   constructor() { }
 
   ngOnInit(): void {
+    // this.dataSource=newItem
+    // this.dataSource = [this.student];
+    
+     console.log(this.dataSource)
+    
   }
 
 }
